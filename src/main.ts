50c3a0cd1d5ba5137cda6bd283601e141372c9d6
TheGood <thegood77@bk.ru>
2024-03-19 20:12:34 +0600

    Add prisma

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api')
+  app.use(cookieParser())
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+    exposedHeaders: 'set-cookie'
+  })
   await app.listen(3000);
 }
 bootstrap();
